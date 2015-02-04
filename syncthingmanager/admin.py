564a4850f0918c8261d2d330903b3649025b9824
Adam M. Peacock <adam@thepeacock.net>
2015-02-04 16:44:57 -0500

    Added basic reporting to admin for the sync thing manager

--- syncthingmanager/admin.py
+++ syncthingmanager/admin.py
@@ -6,14 +6,27 @@ class FolderPathInline(admin.StackedInline):
   filter_horizontal = ["folders"]
   
   extra = 0
-  
+
 class ManagedDeviceAdmin(admin.ModelAdmin):
   inlines = [FolderPathInline]
 
 class SubDeviceAdmin(admin.ModelAdmin):
   filter_horizontal = ["folders"]
+
+class FolderAdmin(admin.ModelAdmin):
+  fields = ['name', 'relative_path', 'folderPaths', 'stubDevices']
+  readonly_fields = ('folderPaths', 'stubDevices')
+
+  def folderPaths(self, o):
+    return ", ".join(map(str, models.FolderPath.objects.filter(folders=o)))
+    
+  def stubDevices(self, o):
+    return ", ".join(map(str, models.StubDevice.objects.filter(folders=o)))
   
+  folderPaths.short_description = "Managed devices"
+  stubDevices.short_description = "Client devices"
+    
 # Register your models here.
 admin.site.register(models.ManagedDevice, ManagedDeviceAdmin)
-admin.site.register(models.Folder)
+admin.site.register(models.Folder, FolderAdmin)
 admin.site.register(models.StubDevice, SubDeviceAdmin)
