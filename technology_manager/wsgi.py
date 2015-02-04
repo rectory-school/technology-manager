512578b41f9fa3b9d7441dbd8566c5e94bc8dce3
Adam M. Peacock <adam@thepeacock.net>
2015-02-04 14:42:10 -0500

    Renamed intake_server to technology_manager

--- technology_manager/wsgi.py
+++ technology_manager/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for intake_server project.
+WSGI config for technology_manager project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "intake_server.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "technology_manager.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
