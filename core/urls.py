e2523285147c23aabae300e6dcc67be786d77e20
Adam M Peacock <adam@thepeacock.net>
2016-12-20 19:00:29 -0500

    Fixed indentation

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
   url(r'^admin/', include(admin.site.urls)),
-	url(r'^munki/', include('munkimanager.urls')),
+  url(r'^munki/', include('munkimanager.urls')),
   url(r'^syncthing/', include('syncthingmanager.urls')),
   url(r'^django-rq/', include('django_rq.urls')),
 )
